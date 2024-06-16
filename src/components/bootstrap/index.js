6fae4884ea080496db37bd326e3c3212c4763a5a
Rhutik Chaudhari <rhutikchaudhari662@gmail.com>
2024-06-16 16:17:47 +0530

    initial commit

---
+++ src/components/bootstrap/index.js
@@ -0,0 +1,1 @@
+export {default as InstallBootstrap} from './installBootStrap'
---
+++ src/components/bootstrap/installBootStrap.tsx
@@ -0,0 +1,15 @@
+'use client'
+import React from 'react'
+import { useEffect } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+const InstallBootStrap = () => {
+    useEffect(()=>{
+        // @ts-ignore
+        import('bootstrap/dist/js/bootstrap.bundle.min.js')
+      },[])
+  return (
+    <></>
+  )
+}
+
+export default InstallBootStrap
